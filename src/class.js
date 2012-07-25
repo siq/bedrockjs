cde083f5c48dda351d1b307ee89b8f3a3e427111
Omer Saeed <omer.saeed@conradlabs.com>
2012-07-25 11:17:40 +0500

    New getAttributeValue / setAttributeValue functions

--- src/class.js
+++ src/class.js
@@ -39,6 +39,7 @@ define([
     };
 
     var Class = function() {};
+        
     var extend = function(namespace, options) {
         var i, mixins, mixin, base = this.prototype;
         if(!options) {
@@ -51,6 +52,38 @@ define([
         inheriting = true;
         var prototype = new this();
         inheriting = false;
+
+        // Create getter / setter methods to emulate string based nested access
+        
+        var getAttributeValue = function(object, name) {
+            var context = object, nesting = name.split("."), index, item;
+            
+            for(index = 0; index < nesting.length-1; ++index) {
+                item = nesting[index];
+                if (!context[item]) {
+                    return;
+                }
+                context = context[item];
+            }
+            return context[nesting[nesting.length-1]];
+        };
+        
+        var setAttributeValue = function(object, name, value) {
+            var context = object, nesting = name.split("."), index, item;
+            
+            for(index = 0; index < nesting.length-1; ++index) {
+                item = nesting[index];
+                if(!context[item]) {
+                    context[item] = {};
+                }
+                context = context[item];
+            }
+            context[nesting[nesting.length-1]] = value;
+        };
+        
+        // Inject that methods into the prototype
+        prototype.getAttributeValue = getAttributeValue;
+        prototype.setAttributeValue = setAttributeValue;
 
         if(mixins) {
             for(i = mixins.length - 1; i >= 0; i--) {
@@ -80,6 +113,7 @@ define([
                 return new constructor({__args__: arguments});
             }
         };
+
 
         constructor.prototype = prototype;
         constructor.constructor = constructor;

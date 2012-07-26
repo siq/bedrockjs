175fa2acb3b4c09df091bc631699c069362dbd82
Omer Saeed <omer.saeed@conradlabs.com>
2012-07-26 14:25:54 +0500

    Changed function signature and made it static as well.

--- src/class.js
+++ src/class.js
@@ -39,7 +39,27 @@ define([
     };
 
     var Class = function() {};
+
         
+    Class.prop = function(object, name, value) {
+        var context = object, nesting = name.split("."), index, item;
+        for(index = 0; index < nesting.length-1; ++index) {
+            item = nesting[index];
+            if (!context[item]) {
+                if(!value) {
+                    return;
+                } else {
+                    context[item] = {};
+                }
+            }
+            context = context[item];
+        }
+        if(value) {
+            context[nesting[nesting.length-1]] = value;
+        }
+        return context[nesting[nesting.length-1]];
+    };
+    
     var extend = function(namespace, options) {
         var i, mixins, mixin, base = this.prototype;
         if(!options) {
@@ -54,36 +74,10 @@ define([
         inheriting = false;
 
         // Create getter / setter methods to emulate string based nested access
-        
-        var getAttributeValue = function(name) {
-            var context = this, nesting = name.split("."), index, item;
-            
-            for(index = 0; index < nesting.length-1; ++index) {
-                item = nesting[index];
-                if (!context[item]) {
-                    return;
-                }
-                context = context[item];
-            }
-            return context[nesting[nesting.length-1]];
+        prototype.prop = function(name, value) {
+            return Class.prop(this, name, value);
         };
         
-        var setAttributeValue = function(name, value) {
-            var context = this, nesting = name.split("."), index, item;
-            
-            for(index = 0; index < nesting.length-1; ++index) {
-                item = nesting[index];
-                if(!context[item]) {
-                    context[item] = {};
-                }
-                context = context[item];
-            }
-            context[nesting[nesting.length-1]] = value;
-        };
-        
-        // Inject that methods into the prototype
-        prototype.getAttributeValue = getAttributeValue;
-        prototype.setAttributeValue = setAttributeValue;
 
         if(mixins) {
             for(i = mixins.length - 1; i >= 0; i--) {

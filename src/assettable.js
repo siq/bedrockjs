0f43d5aa0e2e69acb3fc6cbdc4f13bbe6f3aa022
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-09-24 08:06:58 -0500

    added 'assettable' mixin

---
+++ src/assettable.js
@@ -0,0 +1,289 @@
+define([
+    'vendor/underscore'
+], function(_) {
+    var areNotEquivDates = function(v1, v2) {
+            if (v1 && v1.getDate && v2 && v2.getDate) {
+                return v1.toString() !== v2.toString();
+            }
+            return true;
+        },
+
+        // if you're trying to operate on some nested prop like `foo.bar.baz`,
+        // and you've got an object `{foo: {bar: {baz: 123}}}`, then this will
+        // return the `foo.bar` object.
+        _nestedObj = function(obj, propName) {
+            var i, l, split = propName.split('.'), cur = obj;
+            for (i = 0, l = split.length; i < l-1; i++) {
+                cur = cur[split[i]];
+                if (!_.isObject(cur)) {
+                    return;
+                }
+            }
+            return cur;
+        },
+
+        // if you've got an object `{foo: {bar: {baz: 123}}}`, this will return
+        // some nested prop like `foo.bar.baz`
+        _nested = function(obj, propName, newValue) {
+            var i, l, cur, next = obj, split = propName.split('.');
+            for (i = 0, l = split.length; i < l-1; i++) {
+                cur = next;
+                next = cur[split[i]];
+                if (!_.isObject(next)) {
+                    if (arguments.length > 2) {
+                        cur[split[i]] = next = {};
+                    } else {
+                        return cur;
+                    }
+                }
+            }
+            if (arguments.length > 2) {
+                next[split[split.length-1]] = newValue;
+            } else {
+                return next[split[split.length-1]];
+            }
+        };
+
+    // # asSettable
+    //
+    // functionally mixin standard instance.get() and instance.set() semantics
+    //
+    // also provides a .prop() method that's similar to the jquery function
+    //
+    // ## configuration options
+    //
+    // ### `propName`
+    //
+    // all properties are stored on `this[options.propName]`. this defaults to
+    // `_settableProperties`, so by default calling:
+    //
+    //     instance.set({foo: 'bar'});
+    //
+    // would result in `instance._settableProperties.foo` being set to 'bar'
+    //
+    // IF, however, you wish the properties to be set on someting else, say
+    // for example `this.options`, then call this with the `propName` set to
+    // 'options':
+    //
+    //     var MyClass = MySuperClass.extend({
+    //         // ... etc ...
+    //     });
+    //     asSettable.call(MyClass.prototype, {propName: 'options'});
+    //
+    // if the options define `propName` as `null` (not `undefined`), then the
+    // properties will be set directly on `this`, so for example:
+    //
+    //     var MyClass = MySuperClass.extend({
+    //         // ... etc ...
+    //     });
+    //     asSettable.call(MyClass.prototype, {propName: null});
+    //
+    //     var instance = MyClass().set({foo: 'bar'});
+    //     // instance.foo === bar
+    //
+    // of course, you shouldn't be accessing that object directly anyway,
+    // you should use `.set()`, `.get()`, and `.prop()`.
+    //
+    // ### `eventName`
+    //
+    // if you want to trigger an event when a property changes, override the
+    // `eventName` option at mixin time.
+    //
+    // if you never want an event triggered, just don't define the option.
+    //
+    // ### `onChange`
+    //
+    // if this option is a function, it will be called when a property is
+    // changed. if it is a string, then the corresponding method will be called
+    // on the class's prototype (in the context of the instance of course):
+    //
+    //     var MyClass = MySuperClass.extend({
+    //         onChange: function(changed, opts) {
+    //
+    //             // 'this' refers to the instance of `MyClass`
+    //             this.update(changed);
+    //
+    //             // `opts` is the opts used in the `set` call
+    //             if (!opts.silent) {
+    //                 this.trigger('optionsChange', changed, opts);
+    //             }
+    //     }
+    //     asSettable.call(MyClass.prototype, {onChange: 'onChange'});
+    //
+    //     var MyOtherClass = MySuperClass.extend({
+    //         // ... etc ...
+    //     });
+    //     asSettable.call(MyOtherClass.prototype, {
+    //         onChange: function() {
+    //             /*...*/
+    //         }
+    //     });
+    //
+    // ### 'areEqual'
+    //
+    // if you need to change the comparison function used to determine if two
+    // different values are equal, use the `areEqual` property.
+    //
+    return function(options) {
+        // we use the _settable prop as a proxy object so that when you mixin
+        // to a class, then mixin again to a child class, you retain the
+        // non-overriden options of the parent class' mixin.
+        var _settable = _.extend(this._settable = this._settable || {}, options),
+            p = _settable.propName,
+            eventName = _settable.eventName,
+            onChange = _settable.onChange,
+            areEqual = _settable.areEqual;
+
+        if (typeof p === 'undefined') {
+            p = _settable.propName = '_settableProperties';
+        }
+
+        if (_.isString(onChange)) {
+            onChange = _settable.onChange = this[onChange];
+        }
+
+        this.del = function(key, opts) {
+            var props = p === null? this : this[p];
+            if (!opts || !opts.notNested) {
+                props = _nestedObj(props, key);
+                if (!props) {
+                    return this;
+                }
+                key = key.substring(key.lastIndexOf('.')+1, key.length);
+            }
+            delete props[key];
+            return this;
+        };
+
+        this.get = function(key, opts) {
+            var obj = p === null? this : this[p];
+            return opts && opts.notNested? obj[key] : _nested(obj, key);
+        };
+
+        this.has = function(key, opts) {
+            var props = p === null? this : this[p];
+            if (!opts || !opts.notNested) {
+                props = _nestedObj(props, key);
+                if (!props) {
+                    return false;
+                }
+                key = key.substring(key.lastIndexOf('.')+1, key.length);
+            }
+            return props.hasOwnProperty(key);
+        };
+
+        // return the previous value of a property
+        // return all previous properties if 'key == null'
+        this.previous = function(key) {
+            if (key == null) {
+                return (this._settablePreviousProperties || {});
+            } else {
+                return (this._settablePreviousProperties || {})[key];
+            }
+        };
+
+        // this is either a 'set' or a 'get':
+        //
+        //     instance.prop('stringValueForFirstArg')  // => get
+        //     instance.prop(/* any other args */)      // => set
+        //
+        this.prop = function(/* arguments */) {
+            var isGet = arguments.length === 1 && _.isString(arguments[0]);
+            return this[isGet? 'get' : 'set']
+                .apply(this, arguments);
+        };
+
+        // this can be called in any of the following ways:
+        //
+        //     instance.set({/* props */});
+        //     instance.set({/* props */}, {/* opts */});
+        //     instance.set('foo', bar);
+        //     instance.set('foo', bar, {/* opts */});
+        //
+        // if `opts.silent == true`, then don't trigger a change event
+        // (regardless of whether something changed.
+        //
+        // by default, this function will expand nested properties, i.e.:
+        //
+        //     instance.set('foo.bar', 'baz');
+        //     // -> instance._settableProperties.foo.bar === 'baz'
+        //
+        // to disable this, set `opts.notNested` to true
+        //
+        this.set = function(/* arguments */) {
+            var prop, value, newValue, changed, changing, valuesArentEqual,
+                newProps = {}, opts = {}, changes = {},
+                props = p === null? this : this[p],
+                prevProps = this._settablePreviousProperties;
+
+            if (!props) {
+                props = this[p] = {};
+            }
+
+            if (!prevProps) {
+                prevProps = this._settablePreviousProperties = {};
+            }
+
+            if (arguments.length === 1) {
+                newProps = arguments[0];
+            } else if (arguments.length === 2) {
+                if (_.isString(arguments[0])) {
+                    newProps[arguments[0]] = arguments[1];
+                } else {
+                    newProps = arguments[0];
+                    opts = arguments[1];
+                }
+            } else {
+                newProps[arguments[0]] = arguments[1];
+                opts = arguments[2];
+            }
+
+            // changing = this._settableChanging;
+            // this._settableChanging = true;
+
+            for (prop in newProps) {
+                if (newProps.hasOwnProperty(prop)) {
+                    value = props[prop];
+                    newValue = newProps[prop];
+
+                    valuesArentEqual = areEqual?
+                        !areEqual(value, newValue) :
+                        newValue !== value && areNotEquivDates(value, newValue);
+
+                    if (valuesArentEqual ||
+
+                        // force update to be sent even if there's no changes
+                        opts.update ||
+
+                        // if the prop doesnt exist, but is being set to undef
+                        // (necessary so `.has()` behaves correctly)
+                        !props.hasOwnProperty(prop)) {
+
+                        changes[prop] = changed = true;
+                        if (opts.notNested) {
+                            prevProps[prop] = value;
+                            props[prop] = newValue;
+                        } else {
+                            _nested(prevProps, prop, value);
+                            _nested(props, prop, newValue);
+                        }
+                    }
+                }
+            }
+
+            // if (!changing) {
+                if (changed && !opts.silent) {
+                    if (this.trigger && eventName) {
+                        this.trigger(eventName, changes);
+                    }
+                    if (onChange) {
+                        onChange.call(this, changes, opts);
+                    }
+                }
+                // this._settableChanging = false;
+            // }
+
+            return this;
+        };
+    };
+});
---
+++ src/assettable/test.js
@@ -0,0 +1,350 @@
+/*global test, asyncTest, ok, equal, deepEqual, start, module, strictEqual, notStrictEqual, raises*/
+define([
+    'vendor/underscore',
+    'vendor/moment',
+    './../class',
+    './../events',
+    './../assettable'
+], function(_, moment, Class, Eventable, asSettable) {
+    var MyClass = Class.extend({}),
+        EventableClass = Class.extend({}, {mixins: [Eventable]});
+
+    asSettable.call(MyClass.prototype);
+    asSettable.call(EventableClass.prototype, {eventName: 'change'});
+
+    module('basic functionality');
+
+    test('instantiation', function() {
+        var instance = MyClass();
+        ok(instance);
+    });
+
+    test('setting and getting a property', function() {
+        var instance = MyClass();
+
+        instance.set('foo', 'bar');
+        equal(instance.get('foo'), 'bar');
+
+        instance.set({
+            baz: 'boom',
+            bing: 'bang'
+        });
+        equal(instance.get('baz'), 'boom');
+        equal(instance.get('bing'), 'bang');
+    });
+
+    test('set returns this', function() {
+        var instance = MyClass(),
+            that = instance.set({foo: 'bar'});
+
+        ok(instance === that);
+    });
+
+    test('various forms of calling set', function() {
+        var instance = MyClass();
+
+        instance.set({foo: 'bar', baz: 'boom'});
+        instance.set({the: 'other'}, {silent: true});
+        instance.set('key1', 'value1');
+        instance.set('key2', 'value2', {silent: true});
+
+        equal(instance.get('foo'), 'bar');
+        equal(instance.get('baz'), 'boom');
+        equal(instance.get('the'), 'other');
+        equal(instance.get('key1'), 'value1');
+        equal(instance.get('key2'), 'value2');
+    });
+
+    test('tracking previous values', function() {
+        var instance = MyClass()
+            .set({foo: 'bar', baz: 'boom'})
+            .set({foo: 'bar2', baz: 'boom2'});
+
+        equal(instance.previous('foo'), 'bar');
+        equal(instance.previous('baz'), 'boom');
+        deepEqual(instance.previous(), {foo: 'bar', baz: 'boom'});
+    });
+
+    module('events');
+
+    asyncTest('setting triggers an event', function() {
+        var triggered = false, instance = EventableClass();
+        instance.on('change', function(eventName, changed) {
+            ok(triggered = true, 'event was triggered');
+            ok(changed.foo);
+            start();
+        });
+
+        setTimeout(function() {
+            if (!triggered) {
+                ok(false, 'event wasnt triggered');
+                start();
+            }
+        }, 50);
+
+        instance.set({foo: 'bar'});
+    });
+
+    asyncTest('setting with "silent: true" doesnt trigger', function() {
+        var triggered = false, instance = EventableClass();
+        instance.on('change', function(eventName, changed) {
+            triggered = true;
+            ok(false, 'event was triggered');
+            start();
+        });
+
+        setTimeout(function() {
+            if (!triggered) {
+                ok(true, 'event wasnt triggered');
+                start();
+            }
+        }, 50);
+
+        instance.set({foo: 'bar'}, {silent: true});
+        instance.set('foo', 'bar', {silent: true});
+    });
+
+    module('configuration options');
+
+    test('changing propName', function() {
+        var instance, MyClass = Class.extend({});
+        asSettable.call(MyClass.prototype, {propName: 'options'});
+        instance = MyClass().set({foo: 'bar'});
+
+        ok(typeof instance._settableProperties === 'undefined');
+        equal(instance.options.foo, 'bar');
+
+        instance.set('foo', 'bar2');
+        equal(instance.options.foo, 'bar2');
+        equal(instance.get('foo'), 'bar2');
+        equal(instance.previous('foo'), 'bar');
+    });
+
+    test('setting propName to null', function() {
+        var instance, MyClass = Class.extend({});
+        asSettable.call(MyClass.prototype, {propName: null});
+        instance = MyClass().set({foo: 'bar'});
+
+        ok(typeof instance._settableProperties === 'undefined');
+        equal(instance.foo, 'bar');
+
+        instance.set('foo', 'bar2');
+        equal(instance.foo, 'bar2');
+        equal(instance.get('foo'), 'bar2');
+        equal(instance.previous('foo'), 'bar');
+    });
+
+    asyncTest('changing eventName', function() {
+        var instance, triggered = false,
+            EventableClass = Class.extend({}, {mixins: [Eventable]});
+        asSettable.call(EventableClass.prototype, {eventName: 'foo'});
+        instance = EventableClass();
+
+        instance.on('foo', function(eventName, changed) {
+            ok(triggered = true, 'correct event was triggered');
+            ok(changed.foo);
+            start();
+        }).on('change', function() {
+            triggered = true;
+            ok(false, 'wrong event was triggered');
+            start();
+        });
+
+        setTimeout(function() {
+            if (!triggered) {
+                ok(false, 'event wasnt triggered');
+                start();
+            }
+        }, 50);
+
+        instance.set({foo: 'bar'});
+    });
+
+    asyncTest('changing eventName to null', function() {
+        var instance, triggered = false,
+            EventableClass = Class.extend({}, {mixins: [Eventable]});
+        asSettable.call(EventableClass.prototype, {eventName: null});
+
+        instance = EventableClass();
+        instance.on('foo', function(eventName, changed) {
+            triggered = true;
+            ok(false, 'event should not have triggered');
+            ok(changed.foo);
+            start();
+        });
+
+        setTimeout(function() {
+            if (!triggered) {
+                ok(true, 'no event was triggered');
+                start();
+            }
+        }, 50);
+
+        instance.set({foo: 'bar'});
+
+    });
+
+    asyncTest('setting onChange to a function', function() {
+        var instance, changeFired = false,
+            MyClass = Class.extend({});
+        asSettable.call(MyClass.prototype, {
+            onChange: function(changed, opts) {
+                ok(changeFired = true, 'change happened');
+                ok(this === instance, 'context is set correctly');
+                ok(changed.foo, 'changed object reflects changes');
+                equal(opts.sheezy, 'fo reezy');
+                start();
+            }
+        });
+        instance = MyClass();
+
+        setTimeout(function() {
+            if (!changeFired) {
+                ok(false, 'event wasnt triggered');
+                start();
+            }
+        }, 50);
+
+        instance.set({foo: 'bar'}, {sheezy: 'fo reezy'});
+    });
+
+    asyncTest('setting onChange to a string', function() {
+        var instance, changeFired = false,
+            MyClass = Class.extend({
+                onChange: function(changed, opts) {
+                    ok(changeFired = true, 'change happened');
+                    ok(this === instance, 'context is set correctly');
+                    ok(changed.foo, 'changed object reflects changes');
+                    equal(opts.sheezy, 'fo reezy');
+                    start();
+                }
+            });
+        asSettable.call(MyClass.prototype, {onChange: 'onChange'});
+        instance = MyClass();
+
+        setTimeout(function() {
+            if (!changeFired) {
+                ok(false, 'event wasnt triggered');
+                start();
+            }
+        }, 50);
+
+        instance.set({foo: 'bar'}, {sheezy: 'fo reezy'});
+    });
+
+    asyncTest('setting areEqual works correctly', function() {
+        var instance, v1 = {foo: 'bar'}, v2 = {foo: 'bar'}, changed = false,
+            MyClass = EventableClass.extend({});
+        asSettable.call(MyClass.prototype, {areEqual: _.isEqual});
+        instance = MyClass().set('val', v1);
+
+        instance.on('change', function(evtName, changes) {
+            console.log('in change event, changed:',changes,_.isEmpty(changes));
+            changed = _.isEmpty(changes)? false : changes;
+        }).set('val', v2);
+
+        setTimeout(function() {
+            equal(changed, false);
+
+            instance.set('val', 123);
+
+            setTimeout(function() {
+                deepEqual(changed, {val: true});
+                start();
+            }, 50);
+        }, 50);
+    });
+
+    module('nested properties');
+
+    test('setting/getting nested property', function() {
+        var instance = MyClass().set('foo.bar', 'baz');
+        equal(instance.get('foo.bar'), 'baz');
+        equal(instance._settableProperties.foo.bar, 'baz');
+        ok(_.isObject(instance._settableProperties.foo));
+
+        instance.set('foo', 'baz2');
+        ok(_.isString(instance._settableProperties.foo));
+        equal(instance.get('foo'), 'baz2');
+        equal(instance._settableProperties.foo, 'baz2');
+    });
+
+    module('has');
+
+    test('basic `has` functionality', function() {
+        var m = MyClass().set({foo: 'bar'});
+        equal(m.has('foo'), true);
+        equal(m.has('baz'), false);
+    });
+
+    test('`has` returns true when something set to undefined or null', function() {
+        var m = MyClass().set({foo: undefined, bar: null});
+        equal(m.has('foo'), true);
+        equal(m.has('bar'), true);
+
+        m.set({baz: 'abc'});
+        equal(m.has('baz'), true);
+        m.set({baz: undefined});
+        equal(m.has('baz'), true);
+    });
+
+    test('`has` nested property', function() {
+        var m = MyClass().set('foo.bar', 'baz');
+        ok(m._settableProperties.foo.hasOwnProperty('bar'), 'property is set');
+        ok(m.has('foo.bar'), 'has reports correctly');
+        m.set('foo', 123);
+        equal(m.has('foo.bar'), false, 'has reports correctly');
+    });
+
+    module('dates');
+
+    asyncTest('setting equivalent but different dates doesnt trigger', function() {
+        var d1 = moment(),
+            d2 = d1.clone(),
+            m = EventableClass().set('created', d1.toDate()),
+            triggered = false;
+
+        // sanity check
+        ok(d1.toDate() !== d2.toDate());
+        ok(d1.toDate().toString() === d2.toDate().toString());
+
+        m.on('change', function() {
+            triggered = true;
+        });
+
+        m.set('created', d2.toDate());
+
+        setTimeout(function() {
+            equal(triggered, false, 'setting to an equivalent date shouldnt trigger');
+            start();
+        }, 50);
+    });
+
+    module('delete');
+
+    test('simple deletion of a property', function() {
+        var m = MyClass().set({foo: 'bar'});
+        ok(m.has('foo'), 'property should be present after set');
+        m.set({foo: undefined});
+        ok(m.has('foo'), 'property should be present after setting to undef');
+        m.del('foo');
+        ok(!m.has('foo'), 'property shouldn\'t be present after deletion');
+    });
+
+    test('deletion of a nested property', function() {
+        var m = MyClass().set('foo.bar', 'bar');
+        ok(m.has('foo.bar'), 'property should be present after set');
+        ok(m._settableProperties.foo.hasOwnProperty('bar'), 'nesting');
+        m.set('foo.bar', undefined);
+        ok(m.has('foo'), 'property should be present after setting to undef');
+        ok(m._settableProperties.foo.hasOwnProperty('bar'), 'nesting');
+        m.del('foo.bar');
+        equal(m.has('foo.bar'), false,
+            'property shouldn\'t be present after deletion');
+        ok(!m._settableProperties.foo.hasOwnProperty('bar'), 'nesting');
+    });
+
+    start();
+});
+
+
